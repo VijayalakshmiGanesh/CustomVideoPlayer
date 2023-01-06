e7fa6a9f43112687814bbe0bcca1af6f2285bd32
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-01-06 22:53:29 +0530

    added logic to skip and range sliders

--- main.js
+++ main.js
@@ -2,18 +2,57 @@ let video = document.querySelector("video")
 let playButton = document.querySelector("#playButton")
 let volume = document.querySelector("#volume")
 let playBack = document.querySelector("#playBack")
-let skip = document.querySelectorAll('[data-skip]');
-
+let skip = document.querySelectorAll('[data-skip]')
+let fullScreen = document.querySelector("#fullScreen")
+let progressBar = document.querySelector(".progressFilled")
+let fullScreenFlag = false;
 function PlayPause(){
-   if(video.paused){
-        video.play()
-        playButton.value="❚❚";
-        
-   }else{
-        video.pause()
-        playButton.value="▶"
-   }
+     video.paused? video.play(): video.pause()
 }
 
+function updateButton(){
+     video.paused?playButton.value="▶":playButton.value="❚❚"
+}
+function MakeFullScreen(){
+     if(!fullScreenFlag)
+          video.requestFullscreen();
+     else{
+          if (video.exitFullscreen) {
+               video.exitFullscreen();
+              } else if (video.msExitFullscreen) {
+               video.msExitFullscreen();
+              } else if (video.mozCancelFullScreen) {
+               video.mozCancelFullScreen();
+              } else if (video.webkitExitFullscreen) {
+               video.webkitExitFullscreen();
+              }
+     }
+          
+     fullScreenFlag = !fullScreenFlag
+     console.log(fullScreenFlag)
+     console.log(video.exitFullscreen)
+}
+function VolumeRangleSlider(e){
+    video.volume=this.value
+}
+function playbackRangleSlider(e){
+     video.playbackRate=this.value
+ }
+
+ function skipSecs(){
+     video.currentTime+=parseFloat(this.dataset.skip)
+ }
+function progressBarHandler(){
+     let percent = (video.currentTime/video.duration)*100
+     progressBar.style.flexBasis = `${percent}%`
+     progressBar.style.width=`${percent}%`
+}
 video.addEventListener("click", PlayPause)
 playButton.addEventListener("click", PlayPause)
+video.addEventListener("play", updateButton)
+video.addEventListener("pause", updateButton)
+fullScreen.addEventListener("click",MakeFullScreen)
+skip.forEach(btn => btn.addEventListener("click", skipSecs))
+volume.addEventListener("change", VolumeRangleSlider)
+playBack.addEventListener("change",playbackRangleSlider)
+progressBar.addEventListener("change", progressBarHandler)
